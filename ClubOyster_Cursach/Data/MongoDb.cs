d8dc8847bcb20a16434e366e007eca9571cb3dec
Davletka <106920887+Davletka@users.noreply.github.com>
2023-03-12 19:48:31 +0300

    Добавьте файлы проекта.

---
+++ ClubOyster_Cursach/Data/MongoDb.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using static MongoDB.Bson.Serialization.Serializers.SerializerHelper;
+
+namespace ClubOyster_Cursach.Data
+{
+    public class MongoDb
+    {
+        public static void AddDb(User user)
+        {
+            var client = new MongoClient();
+            var dataBase = client.GetDatabase("ClubOyzer");
+            var collection = dataBase.GetCollection<User>("Users");
+            collection.InsertOne(user);
+        }
+
+        public static List<User> FindUsers()
+        {
+            var client = new MongoClient();
+            var database = client.GetDatabase("ClubOyzer");
+            var collection = database.GetCollection<User>("Users");
+            var list = collection.Find(x => true).ToList();
+
+            return list;
+        }
+    }
+}
---
+++ ClubOyster_Cursach/Data/User.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ClubOyster_Cursach.Data
+{
+    public class User
+    {
+        [BsonId]
+        ObjectId _id;
+
+        [Required (ErrorMessage = "Введите логин")]
+        public string? Login { get; set; }
+
+        [Required(ErrorMessage = "Введите ваше имя")]
+        public string? Name { get; set; }
+
+        [Phone(ErrorMessage = "Введите ваш номер телефона")]
+        public string? Phone { get; set; }
+
+        [EmailAddress (ErrorMessage = "Введите вашу почту")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Введите ваш пароль")]
+        public string? Password { get; set; }
+
+        public string Status { get; set; } = "Empoyee";
+
+    }
+}
